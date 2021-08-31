b0e97ab34d61d29b7e1858f20dd513dcde7de955
IdanHerman12 <idanh156@gmail.com>
2021-08-31 18:58:16 +0300

    bulding GuessCity

--- code.js
+++ code.js
@@ -1,1 +1,23 @@
 
+function guessCity(capital, coastal, famous, ancient) {
+    if(capital && !coastal && famous && ancient)
+    return "jerusalem";
+    else if(!capital && coastal && famous && !ancient ) return "Tel-Aviv";
+    else if (!capital && coastal && !famous && ancient) return "Acre";
+    else if (!capital && !coastal && !famous && ancient)return "Katzrin";
+    else if (!capital && coastal && !famous && !ancient)return "Zikim";
+    else if(!capital && !coastal && !famous && !ancient) return "Musmus";
+    else return "city can not be found";
+
+}
+console.log(guessCity(true,true,false,true));
+
+
+
+
+
+
+
+
+
+
